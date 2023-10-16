ea8510d846b51ef22b11e4b38d3d087d0b0aa9ee
Veeranagouda Ganji <shivakumarganjiatme@gmail.com>
2023-10-16 18:51:08 +0530

    added exepense to Database

--- project.js
+++ project.js
@@ -6,17 +6,30 @@ function storeLocal(e){
     let myObj={
         expenseAmount:document.getElementById('ExpenseAmount').value,
         description:document.getElementById('Description').value,
-        category:document.getElementById('category').value
+        category:document.getElementById('category').value,
+        expenseId:document.getElementById('expenseId').value
     }
-    let key=document.getElementById('ExpenseAmount').value+document.getElementById('Description').value+document.getElementById('category').value
-    localStorage.setItem(key,JSON.stringify(myObj))
-    showOnScreen(myObj)
+    // let key=document.getElementById('ExpenseAmount').value+document.getElementById('Description').value+document.getElementById('category').value
+    // localStorage.setItem(key,JSON.stringify(myObj))
+    axios.
+        post("http://localhost:3000/expense",myObj).
+        then(res=>showOnScreen(res.data)).
+        catch(err=>console.log(err));
 }
+window.addEventListener("DOMContentLoaded",()=>{
+    axios.
+    get("http://localhost:3000/expense").
+    then(res=>{
+        for(var i=0;i<res.data.length;i++){
+            showOnScreen(res.data[i])
+        }
+    }).
+    catch(err=>console.log(err))
+})
 function showOnScreen(obj){
     const parentElement=document.getElementById('list-expense')
     var li=document.createElement('li')
-    var key1=obj.expenseAmount+obj.description+obj.category
-    li.textContent=localStorage.getItem(key1)
+    li.textContent=obj.id+' - '+obj.expenseAmount+' - '+obj.expenseDescription+' - '+obj.category
     var deleteBtn=document.createElement('input')
     deleteBtn.type='button'
     deleteBtn.value='delete'
@@ -24,13 +37,22 @@ function showOnScreen(obj){
     editBtn.type='button'
     editBtn.value='edit'
     deleteBtn.onclick=()=>{
-        localStorage.removeItem(key1)
+        axios.
+            delete(`http://localhost:3000/expense/${obj.id}`).
+            then(result=>console.log(result)).
+            catch(err=>console.log(err))
         parentElement.removeChild(li)
     }
     editBtn.onclick=()=>{
-        localStorage.removeItem(key1)
         parentElement.removeChild(li)
-        li.textContent=localStorage.getItem(key1)
+        document.getElementById('expenseId').value=obj.id
+        document.getElementById('ExpenseAmount').value=obj.expenseAmount
+            document.getElementById('Description').value=obj.expenseDescription
+            document.getElementById('category').value=obj.category
+            axios.
+            delete(`http://localhost:3000/expense/${obj.id}`).
+            then(result=>console.log(result)).
+            catch(err=>console.log(err))
     }
     li.appendChild(deleteBtn)
     li.appendChild(editBtn)
